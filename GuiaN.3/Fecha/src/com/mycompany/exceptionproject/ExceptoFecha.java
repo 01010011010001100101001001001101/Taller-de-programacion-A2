884c01e5f9d8cec557ed85b82257567bf2060153
REV_DEV <149097736+01010011010001100101001001001101@users.noreply.github.com>
2025-03-02 10:32:45 -0500

    GuiaN.3

---
+++ GuiaN.3/Fecha/src/com/mycompany/exceptionproject/ExceptoFecha.java
@@ -0,0 +1,10 @@
+package com.mycompany.exceptionproject;
+/**
+ *
+ * @author Thomas Sanmiguel y Sebastian Revelo
+ */
+public class ExceptoFecha extends Exception {
+    public ExceptoFecha(String s) {
+        super(s);
+    }
+}
